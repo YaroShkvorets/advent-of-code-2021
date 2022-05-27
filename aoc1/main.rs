b7cd5f6d48410c7b003f9ddff298ed783ade8063
YaroShkvorets <shkvorets@gmail.com>
2022-05-26 20:39:30 -0400

    improve complexity

--- aoc1/main.rs
+++ aoc1/main.rs
@@ -1,21 +1,19 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+// 1378
 fn main() {
     let filename = "input.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-    let mut res = -1;
-    let mut last = -1;
-    let mut depths: Vec<i32> = Vec::new();
+    let mut res = 0;
+    let mut depths: Vec<i32> = vec!();
 
     for line in reader.lines() {
         depths.push(line.unwrap().parse().unwrap());
     }
-    for lines in depths.windows(3) {
-        let sum = lines.iter().sum();
-        if sum > last { res = res + 1 }
-        last = sum;
-        println!("window {:?} {}", lines, res)
+    for i in 3..depths.len() {
+        if depths[i] > depths[i-3] { res = res + 1 }
+        println!("window {} {}", i, res)
     }
 }
